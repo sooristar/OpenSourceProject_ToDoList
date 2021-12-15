b6512df82be87e77a427266ff044f43be8ab3c73
sooristar <90605022+sooristar@users.noreply.github.com>
2021-12-16 03:43:08 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {StatusBar,SafeAreaView, Text,Dimensions,View} from 'react-native';
+import {StatusBar,SafeAreaView, Text, Dimensions, ScrollView} from 'react-native';
 import {viewStyles, textStyles} from './styles';
 import Input from './components/Input';
 import {images} from './images';
@@ -8,14 +8,23 @@ import Task from './components/Task';
 
 
 export default function App(){
-
     const width= Dimensions.get('window').width;
 
     const [newTask, setNewTask]= useState('');
 
+    const [tasks, setTasks]= useState({
+        '1':{id:'1',text:"Todo item #1",completed: false},
+        '2':{id:'2',text:"Todo item #2",completed: true},
+    });
+
     const _addTask = () => {
         alert(`Add: ${newTask}`);
+        const ID= Date.now().toString();
+        const newTaskObject={
+            [ID]: {id: ID, text:newTask, completed:false},
+        };
         setNewTask('');
+        setTasks({...tasks,...newTaskObject});
     };
 
     const _handleTextChange = text => {
@@ -28,10 +37,12 @@ export default function App(){
             <Text style={textStyles.title}>TODO LIST</Text> 
             <Input value={newTask} onChangeText={_handleTextChange} onSubmitEditing={_addTask}/>
 
-            <View width= {width -20}>
-                <Task text="Todo item #1"/>
-                <Task text="Todo item #2"/>
-            </View>
+            <ScrollView width= {width-20}>
+                {Object.values(tasks).reverse().map( item=> (
+                <Task key={item.id} text={item.text}/>
+                ))}
+            </ScrollView>
+
         </SafeAreaView>
     );
 }
